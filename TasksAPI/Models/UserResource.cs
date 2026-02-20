bc6996749c578f1d555e3de9bcd7b13a45f32bff
Daniel Baron <baron.daniel.86@gmail.com>
2026-02-20 23:42:50 +0200

    log changes

--- TasksAPI/Models/UserResource.cs
+++ TasksAPI/Models/UserResource.cs
@@ -1,6 +1,17 @@
-﻿namespace TasksAPI.Models
+﻿using System.Runtime.Serialization;
+namespace TasksAPI.Models
 {
-    public sealed record UserResource(int Id, string Username, string Email, string FirstName, string LastName, int UserTypeId, DateTime? CreatedDate, DateTime? UpdatedDate);
+    [DataContract]
+    public sealed record UserResource(
+        [property: DataMember] int Id, 
+        [property: DataMember] string Username, 
+        [property: DataMember] string Email, 
+        [property: DataMember] string FirstName, 
+        [property: DataMember] string LastName, 
+        [property: DataMember] int UserTypeId, 
+        [property: DataMember] DateTime? CreatedDate, 
+        [property: DataMember] DateTime? UpdatedDate);
+    
     public sealed record UserResourceForUpdate(string Username, string Email, string FirstName, string LastName, int UserTypeId);
     public sealed record ClientResourceForUpdate(string Username, string Email, string FirstName, string LastName);
     public record LoginResponse(string Token, UserResource UserProfile, string refreshToken);
